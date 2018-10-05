43d1f35b45d76e3fd2c242b73b59db3bbac2ce05
susanliubc <susanliubc@gmail.com>
2018-10-05 11:53:52 -0700

    add package.json and index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const port = process.env.port || 4000;
+
+// set up express app
+const app = express();
+
+// connect to mongodb
+mongoose.connect("mongodb://localhost/carpoolwithus", {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+
+//set up static files
+app.use(express.static("public"));
+
+// use body-parser middleware
+app.use(bodyParser.json());
+
+// initialize routes
+app.use('/api', require('./routes/api'));
+
+// error handling middleware
+app.use(function(err, req, res, next){
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
+});
+
+// listen for requests
+app.listen(port, () => {
+    console.log("now listening for requests on " + port);
+});
+
